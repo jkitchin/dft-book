aa6161e661bc106730717443b5e1c59b84fc160f
John Kitchin <jkitchin@cmu.edu>
2013-04-25 14:34:01 -0400

    back to a buffer that must be evaluated

--- make.el
+++ make.el
@@ -1,28 +1,29 @@
-(progn
-  (load-file "dft.el")
-  (require 'org-publish)
-  (setq org-publish-project-alist
-	'(
-	  ("dft-book-content"
-	   :base-directory "~/dft-book/"
-	   :base-extension "org"
-	   :publishing-directory "~/dft-book/gh-pages/"
-	   :exclude "gh-pages\\|test\\|archive\\|molecules\\|surfaces\\|bulk\\|study-guides" 
-	   :recursive t
-	   :publishing-function org-publish-org-to-html
-	   :headline-levels 4             ; Just the default for this project.
-	   :auto-preamble t
-	   )
-	  ("dft-book-static"
-	   :base-directory "~/dft-book/"
-	   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg"
-	   :publishing-directory "~/dft-book/gh-pages/"
-	   :exclude  "gh-pages\\|test/\\|archive/\\|molecules/\\|surfaces/\\|bulk/\\|study-guides/"
-	   :recursive t
-	   :publishing-function org-publish-attachment
-	   )
-	  ;; ... all the components ...
-	  ("dft-book" :components ("dft-book-content" "dft-book-static"))
-	  ))
-  (setq org-image-actual-width nil)
-  (org-publish "dft-book"))
+
+(load-file "dft.el")
+(find-file "dft.org")
+(require 'org-publish)
+(setq org-publish-project-alist
+      '(
+	("dft-book-content"
+	 :base-directory "~/dft-book/"
+	 :base-extension "org"
+	 :publishing-directory "~/dft-book/gh-pages/"
+	 :exclude "gh-pages\\|test\\|archive\\|molecules\\|surfaces\\|bulk\\|study-guides" 
+	 :recursive t
+	 :publishing-function org-publish-org-to-html
+	 :headline-levels 4             ; Just the default for this project.
+	 :auto-preamble t
+	 )
+	("dft-book-static"
+	 :base-directory "~/dft-book/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg"
+	 :publishing-directory "~/dft-book/gh-pages/"
+	 :exclude  "gh-pages\\|test/\\|archive/\\|molecules/\\|surfaces/\\|bulk/\\|study-guides/"
+	 :recursive t
+	 :publishing-function org-publish-attachment
+	 )
+	;; ... all the components ...
+	("dft-book" :components ("dft-book-content" "dft-book-static"))
+	))
+(setq org-image-actual-width nil)
+(org-publish "dft-book")
